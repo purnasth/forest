ebba4a5234742d43b849673ac4cbe43949f9e66f
Purna Shrestha <crush9918@gmail.com>
2023-03-29 22:45:47 +0545

    updates

--- js/script.js
+++ js/script.js
@@ -22,21 +22,20 @@ window.addEventListener("scroll", () => {
   stroke.style.marginTop = value * 2.5 + "px";
   leaf.style.top = value * -1.5 + "px";
   leaf.style.left = value * 1.5 + "px";
-    hill5.style.left = value * 1.5 + "px";
-    hill4.style.left = value * -1.5 + "px";
-    hill1.style.top = value * 1 + "px";
+  hill5.style.left = value * 1.5 + "px";
+  hill4.style.left = value * -1.5 + "px";
+  hill1.style.top = value * 1 + "px";
 });
 
 console.log(
   "I have a problem with the code above. I want to make the leaf move to the right and the tree move to the left. But when I add the code for the tree, the leaf stops moving to the right. I don't know why. Can someone help me?"
 );
 
-
 //For navbar
 
 const list = document.querySelectorAll(".list");
-      function activeLink() {
-        list.forEach((item) => item.classList.remove("active"));
-        this.classList.add("active");
-      }
-      list.forEach((item) => item.addEventListener("click", activeLink));
+function activeLink() {
+  list.forEach((item) => item.classList.remove("active"));
+  this.classList.add("active");
+}
+list.forEach((item) => item.addEventListener("click", activeLink));
